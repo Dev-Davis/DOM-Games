ed7337267db50ed54f3bac96c96affb5b9f57075
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-05-08 16:27:07 -0500

    got all of the squares functioning

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 // Restart game button
-const restart = document.querySelector("#buttn");
+const restart = document.querySelector('#buttn');
 // Grab all squares
 const squares = document.querySelectorAll('td');
 // Clear all squares
@@ -11,5 +11,29 @@ function clearBoard() {
 
 restart.addEventListener('click', clearBoard);
 // Check square marker
+// const cellOne = document.querySelector('#one')
+
+// cellOne.addEventListener('click', function() {
+//     if (cellOne.textContent === '') {
+//         cellOne.textContent = 'X';
+//     } else if (cellOne.textContent === 'X') {
+//         cellOne.textContent = 'O';
+//     } else {
+//         cellOne.textContent = '';
+//     }
+// })
+
+function changeMarker() {
+    if (this.textContent === '') {
+        this.textContent = 'X';
+    }else if (this.textContent === 'X') {
+        this.textContent = 'O';
+    } else {
+        this.textContent = '';
+    }
+}
 
 // For loop to add event listeners to all squares
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', changeMarker)
+    }
