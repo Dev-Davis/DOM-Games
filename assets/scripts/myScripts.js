e5914b92ff4d4116cb00c92fa785e6d7e8b03487
Dev Davis <devdavis@Devs-MacBook-Pro.local>
2020-05-09 02:37:14 -0500

    finished t-t-t game

---
+++ assets/scripts/myScripts.js
@@ -0,0 +1,21 @@
+// $('h3').click(function() {
+//     $(this).text("I was changed!")
+// })
+
+// key press
+$('input').eq(0).keypress(function(e) {
+    if (event.which === 13) {
+        $('h3').toggleClass('turnBlue')
+    }
+})
+
+// on() method
+$('h3').on('mouseenter', function() {
+    $(this).toggleClass('turnBlue');
+})
+
+//events and animations
+$('input').eq(1).on('click', function() {
+    $('table').slideToggle(3000);
+})
+
